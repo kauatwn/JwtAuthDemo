878eef0bd6ec36e8a98d90a0fb69a9da436c2b71
kauatwn <kaualopescontato@gmail.com>
2025-07-09 15:39:46 -0300

    Adiciona IAuthService

---
+++ src/JwtAuthDemo.API/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using JwtAuthDemo.API.DTOs.Requests;
+using JwtAuthDemo.API.Models;
+
+namespace JwtAuthDemo.API.Interfaces.Services;
+
+public interface IAuthService
+{
+    Task<AuthResult> LoginAsync(LoginRequest request);
+    Task<AuthResult> RegisterAsync(RegisterRequest request);
+    Task<AuthResult> RefreshTokenAsync(string refreshToken);
+}
