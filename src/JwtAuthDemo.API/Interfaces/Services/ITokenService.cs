266fa77b3da20ca62642ade79c367815c85f42cb
kauatwn <kaualopescontato@gmail.com>
2025-07-09 15:39:38 -0300

    Adiciona ITokenService

---
+++ src/JwtAuthDemo.API/Interfaces/Services/ITokenService.cs
@@ -0,0 +1,12 @@
+﻿using JwtAuthDemo.API.Entities;
+using System.Security.Claims;
+
+namespace JwtAuthDemo.API.Interfaces.Services;
+
+public interface ITokenService
+{
+    ClaimsIdentity GenerateClaimsIdentity(User user);
+    string GenerateAccessToken(ClaimsIdentity claimsIdentity, DateTime expiresAt);
+    string GenerateRefreshToken();
+    ClaimsPrincipal GetPrincipalFromExpiredToken(string expiredToken);
+}
