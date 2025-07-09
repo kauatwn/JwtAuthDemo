1ec747cf84d2a3bc0985675f299c45e26cc5d46e
kauatwn <kaualopescontato@gmail.com>
2025-07-09 15:39:15 -0300

    Adiciona IUserRepository

---
+++ src/JwtAuthDemo.API/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using JwtAuthDemo.API.Entities;
+
+namespace JwtAuthDemo.API.Interfaces.Repositories;
+
+public interface IUserRepository
+{
+    Task<User?> GetByIdAsync(int id);
+    Task<User?> GetByEmailAsync(string email);
+    Task<User?> GetByRefreshTokenAsync(string refreshToken);
+    Task<bool> ExistsByEmailAsync(string email);
+    Task AddAsync(User user);
+    Task SaveChangesAsync();
+}
