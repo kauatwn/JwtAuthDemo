45aa8d0c2f1c44ab55f4ef9021fb295075630808
kauatwn <kaualopescontato@gmail.com>
2025-07-09 15:42:06 -0300

    Adiciona UserService

---
+++ src/JwtAuthDemo.API/Services/UserService.cs
@@ -0,0 +1,13 @@
+﻿using JwtAuthDemo.API.Entities;
+using JwtAuthDemo.API.Interfaces.Repositories;
+using JwtAuthDemo.API.Interfaces.Services;
+
+namespace JwtAuthDemo.API.Services;
+
+public class UserService(IUserRepository repository) : IUserService
+{
+    public async Task<User?> GetByIdAsync(int id)
+    {
+        return await repository.GetByIdAsync(id);
+    }
+}
