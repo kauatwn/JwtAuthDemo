0cfa05564935af7c2fd4cc6d591a088092c63b3a
kauatwn <kaualopescontato@gmail.com>
2025-07-09 15:41:39 -0300

    Adiciona JwtOptions

---
+++ src/JwtAuthDemo.API/Options/JwtOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace JwtAuthDemo.API.Options;
+
+public class JwtOptions
+{
+    public const string SectionName = "JwtOptions";
+
+    public string Issuer { get; init; } = string.Empty;
+    public string Audience { get; init; } = string.Empty;
+    public string SecretKey { get; init; } = string.Empty;
+
+    public TimeSpan AccessTokenExpiration { get; init; }
+    public TimeSpan RefreshTokenExpiration { get; init; }
+}
