d3ad0f5f88e4ad79222dd28183f83014b14923b9
kauatwn <kaualopescontato@gmail.com>
2025-08-04 15:26:21 -0300

    Atualiza User para usar PasswordHash e torna RefreshTokenExpiresAt anulável

--- src/JwtAuthDemo.API/Entities/User.cs
+++ src/JwtAuthDemo.API/Entities/User.cs
@@ -5,7 +5,7 @@ public class User(string name, string email, string password)
     public int Id { get; init; }
     public string Name { get; init; } = name;
     public string Email { get; init; } = email;
-    public string Password { get; init; } = password;
+    public string PasswordHash { get; init; } = password;
     public string RefreshToken { get; set; } = string.Empty;
-    public DateTime RefreshTokenExpiresAt { get; set; } = DateTime.MinValue;
+    public DateTime? RefreshTokenExpiresAt { get; set; }
 }
